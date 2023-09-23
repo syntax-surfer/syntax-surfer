d75cd1bc96a959df78312cc43b67a45f565aca93
Drew Huniak <drewnhuniak@gmail.com>
2023-09-23 13:48:51 -0500

    Built framework for controller

--- Express Backend/server.js
+++ Express Backend/server.js
@@ -4,15 +4,79 @@ const port = 5000;
 
 app.get('/', (req, res) =>
 {
-    res.send("HackMidWest Test");
+    res.send("HackMidWest");
 });
 
-app.get('/api/', (req, res) =>
+app.get('/api/search', async (req, res) =>
 {
+
+    const url = req.query.url;
+    //URL can be blank.
+    const query = req.query.query;
+    if(url) { // URL was given send to Webscraper.
+
+        //Check if the url exists within the Database.
+        try{
+            const data = await checkDatabase(url);
+
+            //If the Data does not exist send to webscraper.
+            if(!data)
+            {
+                //Returns the location of where it is stored.
+                const res = await scrapeContent(url);
+
+                //Send that location to the Embed 
+               
+
+
+            }
+
+        }
+        catch(error)
+        {
+            console.error(error);
+            res.status(500).json({error: 'An error occurred while check the Database.'});
+        }
+        console.log("Send to Webscraper");
+    }
+    else{ //Searching the entire Database with No set DOCs.
+        //
+        console.log("Search Database.");
+    }
+
     res.send("HackMidWest api");
 });
+
+async function checkDatabase(url){
+
+    try
+    {
+        
+    }
+    catch(error)
+    {
+        throw error;
+    }
+
+}
+
+async function scarpeContent(url){
+    try{
+
+    }
+    catch(error)
+    {
+        throw error;
+    }
+}
+
+async function putInDatabase(data)
+{
+
+}
 
 app.listen(port, ()=>
 {
     console.log(`Server is running on port ${port}`);
-})
+});
+
