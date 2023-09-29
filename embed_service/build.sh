792cc75219719093ff20152f843e523c1daa5583
CodyScha <codscha@siue.edu>
2023-09-28 20:18:13 -0500

    more deployment updates

--- embed_service/build.sh
+++
@@ -1,4 +0,0 @@
-docker build -t embed-save .
-aws ecr create-repository --repository-name embed-save-repo
-docker tag embed-save:latest 596108273960.dkr.ecr.us-east-1.amazonaws.com/embed-save-repo:latest
-docker push 596108273960.dkr.ecr.us-east-1.amazonaws.com/embed-save-repo:latest
